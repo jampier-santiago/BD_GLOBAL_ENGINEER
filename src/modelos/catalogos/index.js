fc75012a0d0d0b1ade4832d71117c020d392b7f2
Jampier Santiago Moreno Arenas <67984118+jampier-santiago@users.noreply.github.com>
2022-09-26 22:58:39 -0500

    Change name of variable

--- src/modelos/catalogos/index.js
+++ src/modelos/catalogos/index.js
@@ -1,6 +1,6 @@
 const connection = require("../../conexion");
 
-var catalogosModel = {};
+let catalogosModel = {};
 
 // Traer todos los catalogos
 catalogosModel.getTodosLosCatalogos = (callback) => {
